509e63790dbb08f10ea447071054bc1eab17772f
VivekSharma1897 <viveksharma1897@gmail.com>
2019-11-18 17:41:05 +0530

    Update index.js

--- src/components/profile_page/index.js
+++ src/components/profile_page/index.js
@@ -456,6 +456,11 @@ const HeaderBar = () => (
     <h1 className={classes.navTitle}>
       <Link to={ROUTES.PROFILE_PAGE}>Chatify</Link>
     </h1>
+    <ul className={classes.navList}>
+      <li className={classes.navListItem}>Chat</li>
+      <li className={classes.navListItem}>ProfilePic</li>
+      <li className={classes.navListItem}>Notification</li>
+    </ul>
     <SearchUser />
     <SignOut />
   </div>
